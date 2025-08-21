f852e073edb9c7fbdf138b01a0be46f8f2f5f21d
VISHVESWRA <vishveswravichu@gmail.com>
2025-08-21 16:35:16 +0530

    fix: car list

--- exp.js
+++ exp.js
@@ -1,30 +1,31 @@
 let exp = require('express')
 let bd = require('body-parser')
 let fs = require("fs");
+const cors = require('cors');
 
 let app = exp()
 app.use(bd.json())
 app.use(bd.urlencoded())
+app.use(cors())
 
 
 app.get("/", (req, res) => {
     res.send("welcome")
 })
 app.get("/home", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    console.log('car list');
-
     const oldData = JSON.parse(fs.readFileSync("cars.json", "utf8"));
     res.send(oldData);
 })
 app.get("/read/:pid", (req, res) => {
-
     res.send(req.params.pid)
 })
-app.get("/reads", (req, res) => {
-    res.send(req.query)
+app.post("/home", (req, res) => {
+    // console.log(req.body);
+    const request = req.body
+    const oldList = JSON.parse(fs.readFileSync("cars.json", "utf8"));
+    oldList.push(request);
+    fs.writeFileSync("cars.json", JSON.stringify(oldList, null, 2));
+    // res.send(req.query)
 })
 app.post("/reads", (req, res) => {
     let { name, email, phone } = req.body
